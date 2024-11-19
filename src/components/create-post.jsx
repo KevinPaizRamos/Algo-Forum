7839c06e0f640ff6f2c71f2fe1fe1614bcee67cd
Kevin Paiz Ramos <paizkevin27@gmail.com>
2024-11-18 23:43:43 -0500

    supabse creation

--- src/components/create-post.jsx
+++ src/components/create-post.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { supabase } from "../client";
 
 const CreatePost = () => {
+  const [post, setPost] = useState({
+    title: "",
+    description: "",
+    solution: "",
+    image: "",
+  });
+  const createPost = async (event) => {
+    event.preventDefault();
+
+    await supabase
+      .from("posts")
+      .insert({
+        title: post.title,
+        description: post.description,
+        solution: post.solution,
+        image: post.image,
+      })
+      .select();
+
+    window.location = "/";
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPost((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="create-post">
       <h1>Create Post </h1>
       <form className="post-form">
-        <input type="text" placeholder="Title" />
-        <input type="text" className="description" placeholder="description" />
-        <input type="text" placeholder="solution" />
-        <input type="text" placeholder="Image URL" />
+        <input type="text" placeholder="Title" onChange={handleChange} />
+        <input
+          onChange={handleChange}
+          type="text"
+          className="description"
+          placeholder="description"
+        />
+        <input onChange={handleChange} type="text" placeholder="solution" />
+        <input onChange={handleChange} type="text" placeholder="Image URL" />
       </form>
-      <button>Post</button>
+      <button onSubmit={createPost}>Post</button>
     </div>
   );
 };
